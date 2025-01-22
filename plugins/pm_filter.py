2cb895b656f1c039b188f7ec2d878e42dc0b6e7d
QusaiSoria <ahmad2376dd@gmail.com>
2025-01-22 12:37:03 +0300

    Update pm_filter.py for =

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -141,7 +141,7 @@ async def next_page(bot, query):
         )
         btn.insert(0, [
             InlineKeyboardButton("𝐒𝐞𝐧𝐝 𝐀𝐥𝐥", callback_data=f"sendfiles#{key}"),
-           =
+           
         ])
     try:
         if settings['max_btn']:
