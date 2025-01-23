94c4c66a3a65bcaabe2dacfa589d492fe889d2dd
QusaiSoria <ahmad2376dd@gmail.com>
2025-01-23 11:48:50 +0300

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2717,17 +2717,17 @@ async def auto_filter(client, name, msg, reply_msg, ai_search, spoll=False):
                 cap += f"<b>\n📁 <a href='https://telegram.me/{temp.U_NAME}?start=files_{file['file_id']}'>[{get_size(file['file_size'])}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file['file_name'].split()))}\n</a></b>"
     else:
         if settings["button"]:
-            cap = f"<b>الننائج من  أجل ☞ {search}\n\nصاحب الطلب ☞ {message.from_user.mention}\n\nزمن الإستجابة ☞ {remaining_seconds} sᴇᴄᴏɴᴅs\n\nᴘᴏᴡᴇʀᴇᴅ ʙʏ ☞ : {message.chat.title} \n\n⚠️ سيتم الحذف خلال 5 دقائق 🗑️\n\n</b>"
+            cap = f"<b>هذا ما وجدته لطلبك \n\n</b>"
         else:
-            cap = f"<b>الننائج من  أجل  ☞ {search}\n\nصاحب الطلب  ☞ {message.from_user.mention}\n\nزمن الإستجابة ☞ {remaining_seconds} sᴇᴄᴏɴᴅs\n\nᴘᴏᴡᴇʀᴇᴅ ʙʏ ☞ : {message.chat.title} \n\n⚠️ سيتم الحذف خلال 5 دقائق 🗑️\n\n</b>"
+            cap = f"<b>هذا ما وجدته لطلبك \n\n</b>"
             cap+="<b><u>🍿 ملفاتك 👇</u></b>\n\n"
             for file in files:
                 cap += f"<b>📁 <a href='https://telegram.me/{temp.U_NAME}?start=files_{file['file_id']}'>[{get_size(file['file_size'])}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file['file_name'].split()))}\n\n</a></b>"
-    static_image = 'https://i.postimg.cc/bv88yQC4/IMG-20250122-084536-557.jpg'
+    static_photo = 'https://i.postimg.cc/bv88yQC4/IMG-20250122-084536-557.jpg'
     if imdb and imdb.get('poster'):
     
         try:
-            hehe = await message.reply_photo(photo=static_image, caption=cap, reply_markup=InlineKeyboardMarkup(btn))
+            hehe = await message.reply_photo(photo=static_photo, caption=cap, reply_markup=InlineKeyboardMarkup(btn))
             await reply_msg.delete()
             try:
                 if settings['auto_delete']:
@@ -2742,7 +2742,7 @@ async def auto_filter(client, name, msg, reply_msg, ai_search, spoll=False):
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
             pic = imdb.get('poster')
             poster = pic.replace('.jpg', "._V1_UX360.jpg") 
-            hmm = await message.reply_photo(photo=static_image, caption=cap, reply_markup=InlineKeyboardMarkup(btn))
+            hmm = await message.reply_photo(photo=static_photo, caption=cap, reply_markup=InlineKeyboardMarkup(btn))
             await reply_msg.delete()
             try:
                if settings['auto_delete']:
