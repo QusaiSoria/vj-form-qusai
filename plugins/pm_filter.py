991a962169c92d711d822e7f21d8df25d3b247f1
QusaiSoria <ahmad2376dd@gmail.com>
2025-01-23 20:35:50 +0300

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -83,7 +83,7 @@ async def pm_text(bot, message):
         reply_msg = await bot.send_message(message.from_user.id, f"<b><i>Searching For {content} 🔍</i></b>", reply_to_message_id=message.id)
         await auto_filter(bot, content, message, reply_msg, ai_search)
     else:
-        await message.reply_text(text=f"<b>ʜᴇʏ {user} 😍 ,\n\nلا يمكنك طلب الأفلام والمسلسلات هنا.اشترك في بوت  <a href=https://t.me/cimaparadiso>Cima Paradiso</a> او من هنا 👇</b>", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("📝 اشترك من هنا ", url=f"https://t.me/cimaparadiso")]]))
+        await message.reply_text(text=f"<b>ʜᴇʏ {user} 😍 ,\n\nما فيك تطلب هون.اشترك في بوت  <a href="https://t.me/+5tUz6yhzgt41OTQ0" >المسلسلات السورية</a> او من هنا 👇</b>", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("📝 اشترك من هنا ", url=f"https://t.me/+5tUz6yhzgt41OTQ0")]]))
         await bot.send_message(chat_id=LOG_CHANNEL, text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>")
 
 @Client.on_callback_query(filters.regex(r"^next"))
