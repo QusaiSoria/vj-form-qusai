78dcf43bb0d8954d46b34fdd6c224b2f0605ccff
QusaiSoria <ahmad2376dd@gmail.com>
2025-01-23 11:42:10 +0300

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2725,6 +2725,7 @@ async def auto_filter(client, name, msg, reply_msg, ai_search, spoll=False):
                 cap += f"<b>📁 <a href='https://telegram.me/{temp.U_NAME}?start=files_{file['file_id']}'>[{get_size(file['file_size'])}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file['file_name'].split()))}\n\n</a></b>"
     static_image = 'https://i.postimg.cc/bv88yQC4/IMG-20250122-084536-557.jpg'
     if imdb and imdb.get('poster'):
+    
         try:
             hehe = await message.reply_photo(photo=static_image, caption=cap, reply_markup=InlineKeyboardMarkup(btn))
             await reply_msg.delete()
@@ -2767,7 +2768,7 @@ async def auto_filter(client, name, msg, reply_msg, ai_search, spoll=False):
                 await fek.delete()
                 await message.delete()
     else:
-        fuk = await reply_msg.edit_text(text=cap, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=True)
+        fuk = await reply_msg.edit_text(photo=static_photo,text=cap, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=True)
         
         try:
             if settings['auto_delete']:
